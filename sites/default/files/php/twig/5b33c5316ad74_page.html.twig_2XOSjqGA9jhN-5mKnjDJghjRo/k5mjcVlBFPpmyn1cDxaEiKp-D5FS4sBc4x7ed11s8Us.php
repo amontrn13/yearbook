ea3b4e51a8d53fabc6aff3a5a8328310f6023152
Mario Chinchilla <cs.mchinchilla@gmail.com>
2018-06-29 08:49:59 -0600

    CSS HTML Recognized by Drupal

---
+++ sites/default/files/php/twig/5b33c5316ad74_page.html.twig_2XOSjqGA9jhN-5mKnjDJghjRo/k5mjcVlBFPpmyn1cDxaEiKp-D5FS4sBc4x7ed11s8Us.php
@@ -0,0 +1,115 @@
+<?php
+
+/* themes/custom/yearbook/templates/page.html.twig */
+class __TwigTemplate_ecad869516ebb551e066b0698a6b746c0c0b9b3dde1f64efaa575bfb2f35b65d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<div class=\"layout-container\">
+\t<header role=\"banner\">
+\t\t";
+        // line 3
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "header", array()), "html", null, true));
+        echo "
+\t</header>
+
+\t";
+        // line 6
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "highlighted", array()), "html", null, true));
+        echo "
+
+\t<main role=\"main\">
+
+\t\t<a id=\"main-content\" tabindex=\"-1\"></a>
+\t\t";
+        // line 12
+        echo "
+\t\t";
+        // line 13
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["page"] ?? null), "content", array()), "html", null, true));
+        echo "
+
+\t</main>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/yearbook/templates/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  64 => 13,  61 => 12,  53 => 6,  47 => 3,  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<div class=\"layout-container\">
+\t<header role=\"banner\">
+\t\t{{ page.header }}
+\t</header>
+
+\t{{ page.highlighted }}
+
+\t<main role=\"main\">
+
+\t\t<a id=\"main-content\" tabindex=\"-1\"></a>
+\t\t{# link is in html.html.twig #}
+
+\t\t{{ page.content }}
+
+\t</main>
+</div>
+{# /.layout-container #}", "themes/custom/yearbook/templates/page.html.twig", "C:\\MAMP\\htdocs\\yearbook\\themes\\custom\\yearbook\\templates\\page.html.twig");
+    }
+}
